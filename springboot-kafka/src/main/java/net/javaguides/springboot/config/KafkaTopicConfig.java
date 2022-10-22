71266cb18aa799a5b1eb756a39206cf3204bd54e
yaozeyu112 <3484766443@qq.com>
2022-10-22 11:17:16 +0800

    Initial commit

---
+++ springboot-kafka/src/main/java/net/javaguides/springboot/config/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Bean
+    public NewTopic javaguidesTopic() {
+        return TopicBuilder.name("javaguides").build();
+    }
+}
